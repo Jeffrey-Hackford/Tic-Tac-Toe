4f7f3f10b9541b193ffcfb62eb090f38503d49a2
Jeffrey Hackford and Kellie Olson <jeffrey@>
2016-04-06 14:41:48 -0700

    add player X logic and if statement to declare winner

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,45 @@
 function Player (mark) {
-  var mark = mark;
+  this.mark = mark;
+  this.spaces = [];
+  this.isWinner = false;
 }
 
-function Space (row, column, state) {
-  var row = row;
-  var column = column;
-  var state = state;
+function Space (mark, boardLocation) {
+  //this.row = row;
+  //this.column = column;
+  this.mark = mark;
+  this.boardLocation = boardLocation;
+  //this.markedBy = markedBy;
 }
 
-function Board (spaces) {
-  var spaces = 9;
+function Game (player1, player2, turn, gameOver) {
+  this.player1 = player1;
+  this.player2 = player2;
+  this.turn = 0;
+  this.gameOver = false;
 }
 
-function Game (turn, gameOver) {
-  var turn = 0;
-  var gameOver = false;
-}
 
 $(document).ready(function() {
+  var player1 = new Player("X");
+  var player2 = new Player("O");
 
-$("form#submitButton").submit(function(event) {
+$("form#box-option").submit(function(event) {
     event.preventDefault();
-alert("test");
-  var inputtedBox = $("#spaceInput :selected").val();
 
-  console.log(inputtedBox);
+  var inputtedBox = parseInt($("#spaceInput :selected").val());
+
+  var newSpace = new Space("X", inputtedBox);
+  player1.spaces.push(newSpace);
+  console.log(player1.spaces);
+
+  if (newSpace.boardLocation === (1 && 2 && 3 || 4 && 5 && 6 || 7 && 8 && 9 || 1 && 4 && 7 || 2 && 5 && 8 || 3 && 6 && 9 || 1 && 5 && 9 || 3 && 5 && 7))  {
+    alert("winner!");
+  }
+
+
+
+
 
   });
 });
