cb3ccbc4f8ba6de51f404ae512acb5054eeea067
Jeffrey Hackford and Kellie Olson <kellie@>
2016-04-06 16:14:03 -0700

    add imgs to interface and eliminate already used boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 function Player (mark) {
   this.mark = mark;
   this.spaces = [];
-  this.isWinner = false;
 }
 
 function Space (mark, boardLocation) {
-  //this.row = row;
-  //this.column = column;
   this.mark = mark;
   this.boardLocation = boardLocation;
-  //this.markedBy = markedBy;
 }
 
 function Game (player1, player2, turn, gameOver) {
@@ -24,20 +20,104 @@ $(document).ready(function() {
   var player1 = new Player("X");
   var player2 = new Player("O");
 
+//for player X ---------------------------//
 $("form#box-option").submit(function(event) {
     event.preventDefault();
 
+
   var inputtedBox = parseInt($("#spaceInput :selected").val());
 
-  var newSpace = new Space("X", inputtedBox);
-  player1.spaces.push(newSpace);
+  var newSpaceX = new Space("X", inputtedBox);
+  player1.spaces.push(newSpaceX);
   console.log(player1.spaces);
 
-  if (newSpace.boardLocation === (1 && 2 && 3 || 4 && 5 && 6 || 7 && 8 && 9 || 1 && 4 && 7 || 2 && 5 && 8 || 3 && 6 && 9 || 1 && 5 && 9 || 3 && 5 && 7))  {
-    alert("winner!");
+
+
+  if (newSpaceX.boardLocation === 1) {
+    $(".display1").append("<img src='img/X.png'>");
+    $(".opt1").remove();
+  }
+  if (newSpaceX.boardLocation === 2) {
+    $(".display2").append("<img src='img/X.png'>");
+    $(".opt2").remove();
+  }
+  if (newSpaceX.boardLocation === 3) {
+    $(".display3").append("<img src='img/X.png'>");
+    $(".opt3").remove();
+  }
+  if (newSpaceX.boardLocation === 4) {
+    $(".display4").append("<img src='img/X.png'>");
+    $(".opt4").remove();
+  }
+  if (newSpaceX.boardLocation === 5) {
+    $(".display5").append("<img src='img/X.png'>");
+    $(".opt5").remove();
+  }
+  if (newSpaceX.boardLocation === 6) {
+    $(".display6").append("<img src='img/X.png'>");
+    $(".opt6").remove();
+  }
+  if (newSpaceX.boardLocation === 7) {
+    $(".display7").append("<img src='img/X.png'>");
+    $(".opt7").remove();
+  }
+  if (newSpaceX.boardLocation === 8) {
+    $(".display8").append("<img src='img/X.png'>");
+    $(".opt8").remove();
+  }
+  if (newSpaceX.boardLocation === 9) {
+    $(".display9").append("<img src='img/X.png'>");
+    $(".opt9").remove();
   }
 
+  if (newSpaceX.boardLocation === (1 && 2 && 3 || 4 && 5 && 6 || 7 && 8 && 9 || 1 && 4 && 7 || 2 && 5 && 8 || 3 && 6 && 9 || 1 && 5 && 9 || 3 && 5 && 7))  {
+    alert("winner!");
+  }
+});
 
+//for playerO ------------------------------//
+
+  $("form#box-option2").submit(function(event) {
+      event.preventDefault();
+
+    var inputtedBox = parseInt($("#spaceInput2 :selected").val());
+
+    var newSpaceO = new Space("O", inputtedBox);
+    player2.spaces.push(newSpaceO);
+    console.log(player2.spaces);
+
+    if (newSpaceO.boardLocation === 1) {
+      $(".display1").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 2) {
+      $(".display2").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 3) {
+      $(".display3").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 4) {
+      $(".display4").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 5) {
+      $(".display5").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 6) {
+      $(".display6").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 7) {
+      $(".display7").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 8) {
+      $(".display8").append("<img src='img/O.png'>");
+    }
+    if (newSpaceO.boardLocation === 9) {
+      $(".display9").append("<img src='img/O.png'>");
+    }
+
+
+    if (newSpaceO.boardLocation === (1 && 2 && 3 || 4 && 5 && 6 || 7 && 8 && 9 || 1 && 4 && 7 || 2 && 5 && 8 || 3 && 6 && 9 || 1 && 5 && 9 || 3 && 5 && 7))  {
+      alert("winner!");
+    }
 
 
 
